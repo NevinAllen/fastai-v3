02a69d539380e88a765173de6166a5cd8aec8a57
NevinAllen <53032033+NevinAllen@users.noreply.github.com>
2019-11-27 14:02:06 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,7 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return "It's " + str(prediction)
+    return JSONResponse({"It's " + str(prediction)})
 
 
 if __name__ == '__main__':
