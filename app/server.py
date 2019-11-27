2aab0576f11b194160cc5498c3d101466bf97853
NevinAllen <53032033+NevinAllen@users.noreply.github.com>
2019-11-27 09:13:52 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1L4zMXeryRy1JGd2BWPbKdngU2xfTdqar'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1L4zMXeryRy1JGd2BWPbKdngU2xfTdqar'
 export_file_name = 'export.pkl'
 
 classes = ['bulbasaur', 'charmander', 'pikachu', 'squirtle']
